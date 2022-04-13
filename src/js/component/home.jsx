2ee5ca43c0eda233f694a0083971d02ae913d8d0
marinosig <marinosignorini.it@gmail.com>
2022-04-13 15:18:29 +0000

    first version of a traffic light with react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,52 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("red");
+	const [randonLight, setRandonLight] = useState("light red glow");
+	let listOfLights = ["light red", "light yellow", "light green"];
+	let i = 0;
+
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="stick"></div>
+			<div className="traffic">
+				<div
+					onClick={() => setColor("red")}
+					className={
+						"light red" + (color === "red" ? "light red glow" : "")
+					}></div>
+				<div
+					onClick={() => setColor("yellow")}
+					className={
+						"light yellow" +
+						(color === "yellow" ? "light yellow glow" : "")
+					}></div>
+				<div
+					onClick={() => setColor("green")}
+					className={
+						"light green" +
+						(color === "green" ? "light green glow" : "")
+					}></div>
+			</div>
+			<p></p>
+			<p></p>
+			<button
+				onClick={() =>
+					setInterval(function () {
+						i++;
+						let result = i % listOfLights.length;
+						let traficLight = listOfLights[result] + " glow";
+						console.log(traficLight);
+					}, 1000)
+				}
+				className="changeColor">
+				Cycle Traffic Light Color
+			</button>
 		</div>
 	);
 };
